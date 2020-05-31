2d689a1167272f99ec3cb3e1e02b7f8473d04998
Kalpana Sharma <kalpana.sharma0795@gmail.com>
2020-05-31 22:25:20 +0530

    Use useState hook in App Component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Table from './Table'
 import Form from './Form'
 
-class App extends Component {
-  state = {
-    characters: []
-  }
+function App() {
+  const [characters, setCharacters] = useState([]);
 
-  removeCharacter = (index) => {
-    const characters = this.state.characters
-
-    this.setState({
-      characters: characters.filter((character, i) => {
+  const removeCharacter = (index) => {
+    setCharacters(
+      characters.filter((character, i) => {
         return i !== index
       })
-    })
-  }
-
-  handleSubmit = (character) => {
-    this.setState({
-      characters: [...this.state.characters, character]
-    })
-  }
-
-  render() {
-    const characters = this.state.characters
-
-    return (
-      <div className="container">
-        <h1 className='mt-5'><strong>React Tutorial</strong></h1>
-        <br />
-        <p>Add a team member with a name and a job</p>
-
-        <Table characterData={characters} removeCharacter={this.removeCharacter} />
-
-        <h2 className='mt-5 mb-3'><strong>Add New</strong></h2>
-        <Form handleSubmit={this.handleSubmit} />
-      </div>
     )
   }
+
+  return (
+    <div className="container">
+      <h1 className='mt-5'><strong>React Tutorial</strong></h1>
+      <br />
+      <p>Add a team member with a name and a job</p>
+
+      <Table characterData={characters} removeCharacter={removeCharacter} />
+
+      <h2 className='mt-5 mb-3'><strong>Add New</strong></h2>
+      <Form handleSubmit={(character) => {setCharacters([...characters, character])}} />
+    </div>
+  )
 }
 
 export default App
